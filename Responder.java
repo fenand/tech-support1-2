666df8ac3425b9e22ac4dedb3937877b9b6c5fec
Fenand <garciaf4c@gmail.com>
2018-02-15 20:23:43 +0100

    Actividad 0096

--- Responder.java
+++ Responder.java
@@ -16,7 +16,7 @@ public class Responder
 
     Random respuestaAleatoria;
     ArrayList<String>respuesta;
-    HashMap<String, String> respuestasHashMap;
+    HashMap<HashSet<String>, String> respuestasHashMap;
 
     //crearemos un hashmap para incluir clave y cadenas k son las palabras que va a reconocer string y y v las respuestas
     /**
@@ -37,12 +37,34 @@ public class Responder
 
         //iniciamos el objeto
         respuestasHashMap = new HashMap<>();
-        // llenamos el hashmap
 
-        respuestasHashMap.put("estudiar","deberias estudiar un poco mas");
-        respuestasHashMap.put("adios","te vas ya?");
-        respuestasHashMap.put("no"," no me dejes asi");
-        respuestasHashMap.put("casa","vete de casa pesao");
+        //conjunto de String 
+        HashSet<String> set01 = new HashSet<>();
+        set01.add("adios");
+        set01.add("chao");
+
+        HashSet<String> set02 = new HashSet<>();
+        set02.add("estudiar");
+        set02.add("freak");
+        set02.add("estudiar");
+        set02.add("chao");
+
+        HashSet<String> set03 = new HashSet<>();
+        set03.add("no");
+        set03.add("negativo");
+        set03.add("nulo");
+        set03.add("chao");
+
+        HashSet<String> set04 = new HashSet<>();
+        set04.add("casa");
+        set04.add("home");
+        set04.add("hogar");
+
+        // llenamos el hashmap
+        respuestasHashMap.put(set02,"deberias estudiar un poco mas");
+        respuestasHashMap.put(set01,"te vas ya?");
+        respuestasHashMap.put(set03," no me dejes asi");
+        respuestasHashMap.put(set04,"vete de casa pesao");
 
     }
 
@@ -54,25 +76,26 @@ public class Responder
     {
 
         String respuestaHash = null;
-        Iterator<String> iterator = userInput.iterator();
-        boolean buscador = true;
-        while(iterator.hasNext() && buscador){
-            respuestaHash = respuestasHashMap.get(iterator.next());
-            if(respuestaHash != null){
-                buscador= false;
-            }
-        }
+        // Iterator<String> iterator = userInput.iterator();
+        // boolean buscador = true;
+        // while(iterator.hasNext() && buscador){
+        // respuestaHash = respuestasHashMap.get(iterator.next());
+        // if(respuestaHash != null){
+        // buscador= false;
+        // }
+        // }
 
-        for(String elementos : userInput){
-            //por cada elemto del hasset decir que la respuesta es el valor del hashset que tiene como clave el elemnto que estamos mirando en el for
-            if(respuestaHash== null){
-                respuestaHash = respuestasHashMap.get(elementos);
-            }
-        }
+        // for(String elementos : userInput){
+        // //por cada elemto del hasset decir que la respuesta es el valor del hashset que tiene como clave el elemnto que estamos mirando en el for
+        // if(respuestaHash== null){
+        // respuestaHash = respuestasHashMap.get(elementos);
+        // }
+        // }
 
-        //altenativa con un for
-        String userinputString =  iterator.next();
+        // //altenativa con un for
+        // String userinputString =  iterator.next();
 
+        // respuestaHash = respuestasHashMap.get(userInput);
         respuestaHash = respuestasHashMap.get(userInput);
         int numeroAleatorio =  respuestaAleatoria.nextInt(5);
         if(respuestaHash == null){
